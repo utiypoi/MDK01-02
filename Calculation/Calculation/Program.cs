6dddca7a6c94a7b338cd4d90ed47af02c08aaf81
Ekaterina Zernova <ut26iy00@gmail.com>
2022-10-10 02:56:14 +0500

    Add project MathematicalOperation

---
+++ Calculation/Calculation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения переменных a, b, c, отличные от нуля: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Сложение: " + LibraryMathematicalOperations.MathematicalOperation.AdditionOperation(a,b,c) + 
+                "\nВычитание: " + LibraryMathematicalOperations.MathematicalOperation.SubtractionOperation(a,b,c) + 
+                "\nУмножение: " + LibraryMathematicalOperations.MathematicalOperation.MultiplicationOperation(a, b, c) + 
+                "\nДеление: " + LibraryMathematicalOperations.MathematicalOperation.DivisionOperation(a,b,c) + 
+                "\nВозведние в степень: " + LibraryMathematicalOperations.MathematicalOperation.ExponentiationOperation(a,b,c));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LibraryMathematicalOperations/LibraryMathematicalOperations/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMathematicalOperations
+{
+    public class MathematicalOperation
+    {
+        public static double AdditionOperation(double a, double b, double c)
+        {
+            return(a+b+c);
+        }
+
+        public static double MultiplicationOperation(double a, double b, double c) {
+            return(a*b*c);
+        }
+
+        public static double ExponentiationOperation(double a, double b, double c) {
+            return(Math.Pow(a,Math.Pow(b,c)));
+        }
+
+        public static double SubtractionOperation(double a, double b, double c) {
+            return(a - b - c);
+        }
+
+        public static double DivisionOperation(double a, double b, double c)
+        {
+            return(a/b/c);
+        }
+    }
+}
